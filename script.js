ec2e7d98a9145e88236a263ee42a4e36bb6f8e1b
sarthak-bajaj30 <s.bajaj@student.vu.nl>
2022-05-22 02:00:04 +0200

    levels

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const divItems = document.getElementsByClassName("box");
 let mistakes = 4;
 let levels = [[1,1,1,1,1,0,1,0,1,0,0,0,1,0,0,0,1,0,1,0,1,1,1,1,1],
-    [1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,1,0,1,0,1,1,1,1,1] ];
+[1,0,0,0,0,0,1,0,1,0,0,0,1,0,0,0,1,0,1,0,1,1,1,1,1] ,[1,0,0,0,0,0,1,0,0,0,0,0,1,1,1,0,1,0,0,0,1,0,0,0,0]];
 
 let correctblocks = 15;
 let blankblocks = 25 - correctblocks;
@@ -14,19 +14,75 @@ function gameover() {
 
     mistakes = 4;
     correctblocks = 15;
+    levelcount = 0;
+    currentlevel = levels[0];
+    level1();
+    cleargrid();
+
+}
+
+function cleargrid(){
     $('.active').addClass('box').removeClass('active');
     $('.blank').addClass('box').removeClass('blank');
+    document.getElementById("lives").textContent = "Lives left: " + mistakes;
+}
+
+function level1(){
+    document.getElementById("row1").textContent = "1 1";
+    document.getElementById("row2").textContent = "2 2";
+    document.getElementById("row3").textContent = "1 1 1"
+    document.getElementById("row4").textContent = "2 2";
+    document.getElementById("row5").textContent = "1 1";
+
+    document.getElementById("col1").textContent = "5";
+    document.getElementById("col2").textContent = " 1 \n\r1";
+    document.getElementById("col3").textContent = "1";
+    document.getElementById("col4").textContent = " 1 \n\r1";
+    document.getElementById("col5").textContent = "5";
+
+}
+
+function level2() {
+    document.getElementById("row2").textContent = "1 2";
+    document.getElementById("row3").textContent = "1 1"
+    document.getElementById("row4").textContent = "1 2";
+    document.getElementById("row5").textContent = "1";
+
+    document.getElementById("col1").textContent = "1";
+
+}
+function level3() {
+    document.getElementById("row2").textContent = "1 1";
+    document.getElementById("row3").textContent = "1"
+    document.getElementById("row4").textContent = "1";
+    document.getElementById("row5").textContent = "1";
+
+    document.getElementById("col1").textContent = "1";
+    document.getElementById("col2").textContent = "1";
+    document.getElementById("col3").textContent = "3";
+    document.getElementById("col4").textContent = "1";
+    document.getElementById("col5").textContent = "1";
 
 }
 $(document).ready(function() {
     $(".nxt").click(function(event) {
-        gameover();
-        
-        blankblocks = 25 - correctblocks ;
+        cleargrid();
         levelcount++;
         console.log(levelcount);
         currentlevel = levels[levelcount];
+        if(levelcount == 1){
+            level2();
+        }
+        else if(levelcount == 2) {
+            level3();
+        }
+        else if(levelcount == 3){
+            alert("YOU COMPLETED THE GAME CLICK ON NEXT TO RESTART");
+            gameover();
+        }
+       
         correctblocks = currentlevel.filter(x => x==1).length;
+        blankblocks = 25 - correctblocks;
         console.log(correctblocks);
     })
 });
@@ -46,11 +102,12 @@ $(document).ready(function()
                 event.target.className ="active";
                 correctblocks--;
                 if(correctblocks == 0){
-                    alert("LEVEL 1 cleared");
+                    alert("LEVEL cleared");
                 }
             }
             else {
                 mistakes--;
+                document.getElementById("lives").textContent = "Lives left: " + mistakes;
                 if(mistakes == 0) {
                     alert("game over");
                     gameover();
@@ -68,9 +125,7 @@ $(document).ready(function()
                 event.target.className ="blank";
                 blankblocks--;
             }
-            if(correctblocks == 0 && blankblocks == 0){
-                alert("LEVEL 1 cleared");
-            }
+            if(correctblocks == 0)
            
             break;
         default:
